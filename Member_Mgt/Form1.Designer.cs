e4ab7e9f958c1c4820dda9e243fc65e06a74e397
RONLY_JW <RONLY_JW@DESKTOP-7HSOJHK>
2022-01-14 16:09:38 +0900

    초기 커밋.

---
+++ Member_Mgt/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Member_Mgt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idx = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.mem_id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.mem_name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.mem_gen = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.mem_bdate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.list_mem = new System.Windows.Forms.ListView();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_del = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // idx
+            // 
+            this.idx.Text = "";
+            this.idx.Width = 26;
+            // 
+            // mem_id
+            // 
+            this.mem_id.Text = "아이디";
+            this.mem_id.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.mem_id.Width = 130;
+            // 
+            // mem_name
+            // 
+            this.mem_name.Text = "이름";
+            this.mem_name.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.mem_name.Width = 125;
+            // 
+            // mem_gen
+            // 
+            this.mem_gen.Text = "성별";
+            this.mem_gen.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.mem_gen.Width = 75;
+            // 
+            // mem_bdate
+            // 
+            this.mem_bdate.Text = "생년월일";
+            this.mem_bdate.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.mem_bdate.Width = 80;
+            // 
+            // list_mem
+            // 
+            this.list_mem.BackColor = System.Drawing.Color.White;
+            this.list_mem.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.idx,
+            this.mem_id,
+            this.mem_name,
+            this.mem_gen,
+            this.mem_bdate});
+            this.list_mem.FullRowSelect = true;
+            this.list_mem.HideSelection = false;
+            this.list_mem.Location = new System.Drawing.Point(8, 8);
+            this.list_mem.MultiSelect = false;
+            this.list_mem.Name = "list_mem";
+            this.list_mem.Size = new System.Drawing.Size(440, 426);
+            this.list_mem.TabIndex = 0;
+            this.list_mem.UseCompatibleStateImageBehavior = false;
+            this.list_mem.View = System.Windows.Forms.View.Details;
+            this.list_mem.ColumnWidthChanging += new System.Windows.Forms.ColumnWidthChangingEventHandler(this.list_mem_ColumnWidthChanging);
+            this.list_mem.SelectedIndexChanged += new System.EventHandler(this.list_mem_SelectedIndexChanged);
+            // 
+            // btn_add
+            // 
+            this.btn_add.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_add.Location = new System.Drawing.Point(454, 8);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(40, 40);
+            this.btn_add.TabIndex = 1;
+            this.btn_add.Text = "+";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_del
+            // 
+            this.btn_del.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_del.Location = new System.Drawing.Point(454, 54);
+            this.btn_del.Name = "btn_del";
+            this.btn_del.Size = new System.Drawing.Size(40, 40);
+            this.btn_del.TabIndex = 2;
+            this.btn_del.Text = "-";
+            this.btn_del.UseVisualStyleBackColor = true;
+            this.btn_del.Click += new System.EventHandler(this.btn_del_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_save.Location = new System.Drawing.Point(454, 100);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(40, 40);
+            this.btn_save.TabIndex = 3;
+            this.btn_save.Text = "↵";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_reset.Location = new System.Drawing.Point(454, 146);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(40, 40);
+            this.btn_reset.TabIndex = 4;
+            this.btn_reset.Text = "↻";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(501, 442);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_del);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.list_mem);
+            this.Location = new System.Drawing.Point(500, 100);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "회원관리";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColumnHeader idx;
+        private System.Windows.Forms.ColumnHeader mem_id;
+        private System.Windows.Forms.ColumnHeader mem_name;
+        private System.Windows.Forms.ColumnHeader mem_gen;
+        private System.Windows.Forms.ColumnHeader mem_bdate;
+        private System.Windows.Forms.ListView list_mem;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_del;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_reset;
+    }
+}
+
---
+++ Member_Mgt/Form1.cs
@@ -0,0 +1,239 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Member_Mgt
+{
+    public partial class Form1 : Form
+    {
+        const string ip = "3.35.235.128";
+        const string db = "member_mgt";
+        const string id = "noh99";
+        const string pw = "1111";
+        public string info = $"server={ip};database={db};user={id};password={pw};";
+        public string sql;
+        public List<Control> controls = new List<Control>();
+        public MySqlConnection conn = null;
+        public MySqlCommand cmd = null;
+        public MySqlDataReader rdr = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DataToList();
+        }
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            if (list_mem.FocusedItem != null)
+            {
+                list_mem.SelectedItems.Clear();
+                list_mem.FocusedItem = null;
+            }
+            ControlDispose();
+            ControlAdd();
+        }
+        private void btn_del_Click(object sender, EventArgs e)
+        {
+            if(list_mem.SelectedItems.Count > 0)
+            {
+                string id = list_mem.SelectedItems[0].SubItems[1].Text;
+                DialogResult check = MessageBox.Show("정말로 삭제하시겠습니까?", id,MessageBoxButtons.YesNo);
+
+                if(check == DialogResult.Yes)
+                {
+                    try
+                    {
+                        using (conn = new MySqlConnection(info))
+                        {
+                            conn.Open();
+                            sql = $"DELETE FROM tbl_mem WHERE id = '{id}'";
+                            cmd = new MySqlCommand(sql, conn);
+                            cmd.ExecuteNonQuery();
+                        }
+                        ControlDispose();
+                        DataToList();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, sql);
+                    }
+                }
+            }      
+        }
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            if(controls.Count > 0)
+            {
+                try
+                {
+                    string id = controls[0].Text;
+                    if (id == "") throw new Exception("아이디를 입력해주세요.");
+                    string nm = controls[1].Text;
+                    if (nm == "") throw new Exception("이름을 입력해주세요.");
+                    string gen;
+                    if (((RadioButton)controls[2]).Checked) gen = controls[2].Text;
+                    else gen = controls[3].Text;
+                    string bd = controls[4].Text;
+
+                    using (conn = new MySqlConnection(info))
+                    {
+                        conn.Open();
+
+                        if(list_mem.SelectedItems.Count == 0)
+                        {
+                            sql = $"SELECT * FROM tbl_mem WHERE id = '{id}'";
+                            cmd = new MySqlCommand(sql, conn);
+                            rdr = cmd.ExecuteReader();
+
+                            if (rdr.HasRows) throw new Exception("이미 존재하는 아이디입니다.");
+                            rdr.Close();
+
+                            sql = $"INSERT INTO tbl_mem(id, nm, gen, bd) VALUES('{id}', '{nm}', '{gen}', '{bd}')";
+                            cmd = new MySqlCommand(sql, conn);
+                            cmd.ExecuteNonQuery();
+                        }
+                        else
+                        {
+                            sql = $"UPDATE tbl_mem SET nm = '{nm}', gen = '{gen}', bd = '{bd}' WHERE id = '{id}'";
+                            cmd = new MySqlCommand(sql, conn);
+                            cmd.ExecuteNonQuery();
+                        }        
+                    }
+                    ControlDispose();
+                    DataToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "오류");
+                }
+            }
+        }
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            ControlDispose();
+            DataToList();
+        }
+        private void list_mem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (list_mem.SelectedItems.Count > 0)
+            {
+                ControlDispose();
+                ControlAdd();
+
+                controls[0].Text = list_mem.SelectedItems[0].SubItems[1].Text;
+                ((TextBox)controls[0]).ReadOnly = true;
+                ((TextBox)controls[0]).BackColor = Color.White;
+                ((TextBox)controls[0]).ForeColor = Color.DarkGray;
+                controls[1].Text = list_mem.SelectedItems[0].SubItems[2].Text;
+                if (controls[2].Text == list_mem.SelectedItems[0].SubItems[3].Text) 
+                    ((RadioButton)controls[2]).Checked = true;
+                else 
+                    ((RadioButton)controls[3]).Checked = true;
+                controls[4].Text = list_mem.SelectedItems[0].SubItems[4].Text;
+            }
+        }
+        private void list_mem_ColumnWidthChanging(object sender, ColumnWidthChangingEventArgs e)
+        {
+            e.NewWidth = list_mem.Columns[e.ColumnIndex].Width;
+            e.Cancel = true;
+        }
+        public void DataToList()
+        {
+            list_mem.Groups.Clear();
+            list_mem.Items.Clear();
+
+            list_mem.BeginUpdate();
+
+            using (conn = new MySqlConnection(info))
+            {
+                conn.Open();
+
+                sql = "SELECT * FROM tbl_mem";
+                cmd = new MySqlCommand(sql, conn);
+                rdr = cmd.ExecuteReader();
+
+                DataTable dt = new DataTable();
+                dt.Load(rdr);
+
+                for (int i = 1; i <= dt.Rows.Count; i++)
+                {
+                    ListViewItem item = new ListViewItem(i.ToString());
+                    item.SubItems.Add(dt.Rows[i - 1][1].ToString());
+                    item.SubItems.Add(dt.Rows[i - 1][2].ToString());
+                    item.SubItems.Add(dt.Rows[i - 1][3].ToString());
+                    item.SubItems.Add(dt.Rows[i - 1][4].ToString().Split(' ')[0]);
+                    list_mem.Items.Add(item);
+                }
+            }
+            list_mem.EndUpdate();
+        }
+        public void ControlAdd()
+        {
+            int last = list_mem.Items.Count - 1;
+
+            if (last < 0 || list_mem.Items[last].Text != "")
+            {
+                ListViewItem item = new ListViewItem();
+                item.SubItems.Add("");
+                item.SubItems.Add("");
+                item.SubItems.Add("");
+                item.SubItems.Add("");
+                list_mem.Items.Add(item);
+
+                last++;
+
+                TextBox tb1 = new TextBox();
+                TextBox tb2 = new TextBox();
+                RadioButton rb1 = new RadioButton();
+                RadioButton rb2 = new RadioButton();
+                DateTimePicker dtp = new DateTimePicker();
+
+                rb1.Text = "남";
+                rb2.Text = "여";
+                rb1.Checked = true;
+
+                dtp.CustomFormat = "yyyy-MM-dd";
+                dtp.Format = DateTimePickerFormat.Custom;
+
+                tb1.Parent = list_mem;
+                tb2.Parent = list_mem;
+                rb1.Parent = list_mem;
+                rb2.Parent = list_mem;
+                dtp.Parent = list_mem;
+
+                Rectangle rt;
+
+                rt = list_mem.Items[last].SubItems[1].Bounds;
+                tb1.SetBounds(rt.X, rt.Y, rt.Width, rt.Height);
+                controls.Add(tb1);
+
+                rt = list_mem.Items[last].SubItems[2].Bounds;
+                tb2.SetBounds(rt.X, rt.Y, rt.Width, rt.Height);
+                controls.Add(tb2);
+
+                rt = list_mem.Items[last].SubItems[3].Bounds;
+                rb1.SetBounds(rt.X + 5, rt.Y + 2, rt.Width / 2 - 5, rt.Height);
+                controls.Add(rb1);
+                rb2.SetBounds(rt.X + 5 + rt.Width / 2, rt.Y + 2, rt.Width / 2 - 5, rt.Height);
+                controls.Add(rb2);
+
+                rt = list_mem.Items[last].SubItems[4].Bounds;
+                dtp.SetBounds(rt.X, rt.Y, rt.Width, rt.Height);
+                controls.Add(dtp);
+
+                list_mem.Items.RemoveAt(last);
+            }
+        }
+        public void ControlDispose()
+        {
+            foreach (Control control in controls)
+            {
+                control.Dispose();
+            }
+            controls.Clear();
+        }
+    }
+}
